b13711a57e618132f1701d8195fd22ba7e7c17b1
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2023-06-29 20:47:38 -0300

    add methods: length, insertFirst, insertFinal

--- list/SinglyLinkedList.java
+++ list/SinglyLinkedList.java
@@ -1,33 +1,62 @@
 public class SinglyLinkedList {
+    
     private ListNode head;
+    
     private static class ListNode {
+
         private int data;
         private ListNode next;
+
         public ListNode(int data) {
+            
             this.data = data;
             this.next = null;
         }
     }
     public static void main(String[] args) {
-        SinglyLinkedList singlyL = new SinglyLinkedList();
-        singlyL.head = new ListNode(10);
-        ListNode second = new ListNode(1);
-        ListNode third = new ListNode(8);
-        ListNode fourth = new ListNode(11);
-
-        singlyL.head.next = second;
-        second.next = third;
-        third.next = fourth;
-
-        singlyL.printList();
-        System.out.println("Hello world!");
+        SinglyLinkedList list = new SinglyLinkedList();
+        list.insertFinal(10);
+        list.insertFirst(5);
+        list.print();
+        System.out.println(list.length());
     }
-    public void printList() {
+    public void print() {
         ListNode current = head;
         while (current != null) {
-            System.out.print("|"+current.data);
+            System.out.print("|"+ current.data);
             current = current.next;
         }
         System.out.println("|null");
+    }
+    public int length(){
+        if(head == null){
+            return 0;
+        }
+        int count = 0;
+        ListNode current = head;
+        while(current != null){
+            count++;
+            current = current.next;
+        }
+         return count;
+    }
+    public void insertFirst(int value){
+        ListNode newNode = new ListNode(value);
+        newNode.next = head;
+        head = newNode;
+
+    }
+
+    public void insertFinal(int value){
+        ListNode newNode = new ListNode(value);
+        if(head == null){
+            head = newNode;
+            return;
+        }
+        ListNode current = head;
+        while (current.next != null){
+            current = current.next;
+        }
+        current.next = newNode;
     }
 }
