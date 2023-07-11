e3efb4d09791e1cde880998e8a2563062aa1ffd3
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2023-07-11 15:36:05 -0300

    SinglyLinkedList

--- list/SinglyLinkedList.java
+++ list/SinglyLinkedList.java
@@ -1,24 +1,25 @@
 public class SinglyLinkedList {
-    
+    public static void main(String[] args) {
+        SinglyLinkedList list = new SinglyLinkedList();
+        list.insertFinal(10);
+        list.insertFirst(5);
+        list.print();
+        // System.out.println(list.deleteFirst().data);
+        System.out.println(list.deleteLast().data);
+        list.print();
+    }
+
     private ListNode head;
-    
     private static class ListNode {
 
         private int data;
         private ListNode next;
 
         public ListNode(int data) {
-            
+
             this.data = data;
             this.next = null;
         }
-    }
-    public static void main(String[] args) {
-        SinglyLinkedList list = new SinglyLinkedList();
-        list.insertFinal(10);
-        list.insertFirst(5);
-        list.print();
-        System.out.println(list.length());
     }
     public void print() {
         ListNode current = head;
@@ -28,35 +29,57 @@ public class SinglyLinkedList {
         }
         System.out.println("|null");
     }
-    public int length(){
-        if(head == null){
+    public int length() {
+        if(head == null) {
             return 0;
         }
         int count = 0;
         ListNode current = head;
-        while(current != null){
+        while(current != null) {
             count++;
             current = current.next;
         }
-         return count;
+        return count;
     }
-    public void insertFirst(int value){
+    public void insertFirst(int value) {
         ListNode newNode = new ListNode(value);
         newNode.next = head;
         head = newNode;
 
     }
-
-    public void insertFinal(int value){
+    public void insertFinal(int value) {
         ListNode newNode = new ListNode(value);
-        if(head == null){
+        if(head == null) {
             head = newNode;
             return;
         }
         ListNode current = head;
-        while (current.next != null){
+        while (current.next != null) {
             current = current.next;
         }
         current.next = newNode;
+    }
+    public ListNode deleteFirst() {
+        if (head == null) {
+            return null;
+        }
+        ListNode temp = head;
+        head = head.next;
+        temp.next = null;
+        return head;
+    }
+    public ListNode deleteLast() {
+        if(head == null || head.next == null) {
+            return head;
+        }
+        ListNode current = head;
+        ListNode previous = null;
+        while(current.next != null) {
+            previous = current;
+            current = current.next;
+        }
+
+        previous.next = null;
+        return current;
     }
 }
