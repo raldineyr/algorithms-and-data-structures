3e708b92dc0302515c961b1f8fda70ba7a5336f4
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2023-08-10 18:54:56 -0300

    HashTable

--- HashTable/HashTable.java
+++ HashTable/HashTable.java
@@ -1,5 +1,10 @@
-
 public class HashTable {
+    public static void main(String[] args){
+        HashTable table = new HashTable(10);
+        table.put(105,"RALDINEY");
+        table.put(21,"MARIO");
+        System.out.println(table.size());
+    }
     private HashNode[] buckets;
     private int numOfBuckets;
     private int size;
@@ -16,16 +21,40 @@ public class HashTable {
         private Integer key;
         private String value;
         private HashNode next;
+
         public HashNode(Integer key, String value){
             this.key = key;
             this.value = value;
         }
     }
-
     public int size(){
         return size;
     }
     public boolean isEmpty(){
         return size == 0;
+    }
+
+    public void put(Integer key, String value){
+        if(key == null || value == null ){
+            throw new IllegalArgumentException("A chave ou o valor não podem ser nulos!");
+        }
+        int bucketIndex = getBucketIndex(key);
+        HashNode head = buckets[bucketIndex];
+        while (head != null){
+            if(head.key.equals(key)){
+                head.value = value;
+                return ;
+            }
+            head = head.next;
+        }
+        size++;
+        head = buckets[bucketIndex];
+        HashNode node = new HashNode(key, value);
+        node.next = head;
+        buckets[bucketIndex] = node;
+    }
+
+    public int getBucketIndex(Integer key){
+        return key % numOfBuckets;
     }
 }
