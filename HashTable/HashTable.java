74f810e624d9583dc0fc2d279b2381d2b2e861fe
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2023-08-10 10:57:42 -0300

    HashTable

--- HashTable/HashTable.java
+++ HashTable/HashTable.java
@@ -1,7 +1,12 @@
+
 public class HashTable {
     private HashNode[] buckets;
     private int numOfBuckets;
     private int size;
+
+    public HashTable(){
+        this(10);
+    }
 
     public HashTable (int capacity){
         this.numOfBuckets = capacity;
@@ -11,10 +16,16 @@ public class HashTable {
         private Integer key;
         private String value;
         private HashNode next;
-      
         public HashNode(Integer key, String value){
             this.key = key;
             this.value = value;
         }
+    }
+
+    public int size(){
+        return size;
+    }
+    public boolean isEmpty(){
+        return size == 0;
     }
 }
