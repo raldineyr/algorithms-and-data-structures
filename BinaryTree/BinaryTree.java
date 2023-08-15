7823e44f025ae4e34df854a97511118b035bcc09
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2023-08-15 15:02:16 -0300

    BinaryTree

---
+++ BinaryTree/BinaryTree.java
@@ -0,0 +1,13 @@
+public class BinaryTree {
+    private TreeNode root;
+
+    private class TreeNode{
+        private TreeNode left;
+        private TreeNode right;
+        private int data;
+
+        public TreeNode(int data){
+            this.data = data;
+        }
+    }
+}
