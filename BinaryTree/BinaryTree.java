07f9fc30966e4f7bc40921f1e0944496bc5768a1
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2023-08-17 08:49:47 -0300

    BinaryTree

--- BinaryTree/BinaryTree.java
+++ BinaryTree/BinaryTree.java
@@ -1,4 +1,11 @@
+
 public class BinaryTree {
+    public static void main(String[] args){
+        BinaryTree binarytree = new BinaryTree();
+        binarytree.createBinaryTree();
+        binarytree.preOrder(binarytree.root);
+
+    }
     private TreeNode root;
 
     private class TreeNode{
@@ -9,5 +16,22 @@ public class BinaryTree {
         public TreeNode(int data){
             this.data = data;
         }
+    }
+    public void createBinaryTree(){
+        TreeNode first = new TreeNode(9);
+        TreeNode second = new TreeNode(2);
+        TreeNode third = new TreeNode(3);
+        TreeNode fourth = new TreeNode(4);
+
+        root = first;
+        first.left = second;
+        first.right = third;
+        second.left = fourth;
+    }
+    public void preOrder(TreeNode root){
+        if(root == null){return;}
+        System.out.println(root.data + " ");
+        preOrder(root.left);
+        preOrder(root.right);
     }
 }
